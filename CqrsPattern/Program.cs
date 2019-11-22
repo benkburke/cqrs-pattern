c9be6b5e0b948ac8548483c6bb0edc11228399e1
Ben Burke <benkburke@gmail.com>
2019-11-22 12:27:05 -0800

    init

---
+++ CqrsPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CqrsPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
